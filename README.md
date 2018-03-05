e915e85c8ea64bc5febb67d6432e2eecb67e458e
MouseyPounds <mouseypounds+github@gmail.com>
2018-03-04 20:49:28 -0500

    rewrite CSRandom "class" to avoid ES2015 features

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This app simulates the random number generator used in Stardew Valley and makes 
 The app is written in Javascript and uses jQuery it is hosted on GitHub Pages at https://mouseypounds.github.io/stardew-checkup/ and the source code repository is https://github.com/MouseyPounds/stardew-checkup. It is released under the MIT license.
 
 Changelog
+*  4 Mar  2018 - v0.94 - Rewrite of RNG class to avoid ES2015-specific features for Pale Moon compatibility
 *  3 Mar  2018 - v0.93 - Indicator for geode contents which need donation
 * 25 Feb  2018 - v0.92 - Fixing Baryte spelling and double-button processing bug
 * 22 Feb  2018 - v0.91 - Search for Cart items, Winter Star bugfix
--- cs-random.js
+++ cs-random.js
@@ -5,127 +5,136 @@
  * published at http://referencesource.microsoft.com/#mscorlib/system/random.cs
  */
 
-/*jshint browser: true, jquery: true, esnext: true */
+/*jshint browser: true, jquery: true, esnext: false */
 
-const INT_MIN = -2147483648,
-		INT_MAX = 2147483647,
-		MBIG = INT_MAX,
-		MSEED = 161803398;
+// These would be constants in ES2015
+var INT_MIN = -2147483648,
+	INT_MAX = 2147483647,
+	MBIG = INT_MAX,
+	MSEED = 161803398;
 
-class CSRandom {
-	constructor(Seed = Date.getTime()) {
-		var ii, mj, mk, i, k, subtraction;
-		Seed = parseInt(Seed); // Force an integer since there is no type checking
-		
-		this.inext = 0;
-		this.inextp = 0;
-		this.SeedArray = [];		
-		
-		subtraction = (Seed === INT_MIN) ? INT_MAX : Math.abs(Seed);
-		mj = MSEED - subtraction;
-		this.SeedArray[55] = mj;
-		mk = 1;
-		for (i = 1; i < 55; i++) {
-			ii = (21 * i) % 55;
-			this.SeedArray[ii] = mk;
-			mk = mj - mk;
-			if (mk < 0) {
-				mk += MBIG;
-			}
-			mj = this.SeedArray[ii];
-		}
-		for (k = 1; k < 5; k++) {
-			for (i = 1; i < 56; i++) {
-				this.SeedArray[i] -= this.SeedArray[1 + (i + 30) % 55];
-				if (this.SeedArray[i] < 0) {
-					this.SeedArray[i] += MBIG;
-				}
-			}
-		}
-		this.inext = 0;
-		this.inextp = 21;
-		Seed = 1;
+function CSRandom(Seed) {
+	"use strict";
+	var ii, mj, mk, i, k, subtraction;
+
+	// Alternative to default argument
+	if (typeof(Seed) === 'undefined') {
+		Seed = Date.getTime();
 	}
-	
-	Sample() {
-		return parseFloat(this.InternalSample() * (1.0 / MBIG));
-	}
-	
-	InternalSample() {
-		var retVal,
-			locINext = this.inext,
-			locINextp = this.inextp;
+	Seed = parseInt(Seed); // Force an integer since there is no type checking
 		
-		if (++locINext >= 56) {
-			locINext = 1;
-		}
-		if (++locINextp >= 56) {
-			locINextp = 1;
-		}
-		retVal = this.SeedArray[locINext] - this.SeedArray[locINextp];
-		if (retVal === MBIG) {
-			retVal--;
-		}
-		if (retVal < 0) {
-			retVal += MBIG;
-		}
-		this.SeedArray[locINext] = retVal;
-		this.inext = locINext;
-		this.inextp = locINextp;
-		return parseInt(retVal);
-	}
-	
-	GetSampleForLargeRange() {
-		// This might require special large integer handling
-		var result = this.InternalSample(),
-			d;
+	this.inext = 0;
+	this.inextp = 0;
+	this.SeedArray = [];	
 			
-		if (this.InternalSample() %2 === 0) {
-			result = -result;
+	subtraction = (Seed === INT_MIN) ? INT_MAX : Math.abs(Seed);
+	mj = MSEED - subtraction;
+	this.SeedArray[55] = mj;
+	mk = 1;
+	for (i = 1; i < 55; i++) {
+		ii = (21 * i) % 55;
+		this.SeedArray[ii] = mk;
+		mk = mj - mk;
+		if (mk < 0) {
+			mk += MBIG;
 		}
-		d = result;
-		d += (INT_MAX - 1);
-		d /= 2 * INT_MAX - 1;
-		return d;
+		mj = this.SeedArray[ii];
 	}
-	
-	Next(a, b) {
-		// Next() gives range of [0..INT_MAX)
-		// Next(a) gives range of [0..a)
-		// Next(a,b) gives range of [a..b)
-		var min = 0,
-			max = INT_MAX,
-			range;
-
-		if (typeof b !== 'undefined') {
-			// 2 parameter version
-			max = b;
-			min = (typeof a !== 'undefined') ? a : 0;
-			if (min > max) {
-				throw "Argument out of range - min (" + min + ") should be smaller than max (" + max + ")";
+	for (k = 1; k < 5; k++) {
+		for (i = 1; i < 56; i++) {
+			this.SeedArray[i] -= this.SeedArray[1 + (i + 30) % 55];
+			if (this.SeedArray[i] < 0) {
+				this.SeedArray[i] += MBIG;
 			}
-			range = max - min;
-			if (range <= INT_MAX) {
-				return parseInt(this.Sample() * range + min);
-			} else {
-				return parseInt(this.GetSampleForLargeRange() * range + min);
-			}
-		} else if (typeof a !== 'undefined') {
-			// 1 parameter version
-			max = a;
-			if (max < 0) {
-				throw "Argument out of range - max (" + max + ") must be positive";
-			}
-			return parseInt(this.Sample() * max);
-		} else {
-			return this.InternalSample();
 		}
 	}
-		
-	NextDouble() {
-		return this.Sample();
-	}
-	
-	// not implementing NextBytes since Stardew Valley doesn't use it
-		
+	this.inext = 0;
+	this.inextp = 21;
+	Seed = 1;
 }
+
+CSRandom.prototype.Sample = function() {	
+	"use strict";
+	return parseFloat(this.InternalSample() * (1.0 / MBIG));
+};
+	
+CSRandom.prototype.InternalSample = function() {
+	"use strict";
+	var retVal,
+		locINext = this.inext,
+		locINextp = this.inextp;
+	
+	if (++locINext >= 56) {
+		locINext = 1;
+	}
+	if (++locINextp >= 56) {
+		locINextp = 1;
+	}
+	retVal = this.SeedArray[locINext] - this.SeedArray[locINextp];
+	if (retVal === MBIG) {
+		retVal--;
+	}
+	if (retVal < 0) {
+		retVal += MBIG;
+	}
+	this.SeedArray[locINext] = retVal;
+	this.inext = locINext;
+	this.inextp = locINextp;
+	return parseInt(retVal);
+};
+	
+CSRandom.prototype.GetSampleForLargeRange = function() {
+	"use strict";
+	// This might require special large integer handling
+	var result = this.InternalSample(),
+		d;
+		
+	if (this.InternalSample() %2 === 0) {
+		result = -result;
+	}
+	d = result;
+	d += (INT_MAX - 1);
+	d /= 2 * INT_MAX - 1;
+	return d;
+};
+	
+CSRandom.prototype.Next = function(a, b) {
+	"use strict";
+	// Next() gives range of [0..INT_MAX)
+	// Next(a) gives range of [0..a)
+	// Next(a,b) gives range of [a..b)
+	var min = 0,
+		max = INT_MAX,
+		range;
+
+	if (typeof b !== 'undefined') {
+		// 2 parameter version
+		max = b;
+		min = (typeof a !== 'undefined') ? a : 0;
+		if (min > max) {
+			throw "Argument out of range - min (" + min + ") should be smaller than max (" + max + ")";
+		}
+		range = max - min;
+		if (range <= INT_MAX) {
+			return parseInt(this.Sample() * range + min);
+		} else {
+			return parseInt(this.GetSampleForLargeRange() * range + min);
+		}
+	} else if (typeof a !== 'undefined') {
+		// 1 parameter version
+		max = a;
+		if (max < 0) {
+			throw "Argument out of range - max (" + max + ") must be positive";
+		}
+		return parseInt(this.Sample() * max);
+	} else {
+		return this.InternalSample();
+	}
+};
+		
+CSRandom.prototype.NextDouble = function() {
+	"use strict";
+	return this.Sample();
+};
+	
+// not implementing NextBytes since Stardew Valley doesn't use it
